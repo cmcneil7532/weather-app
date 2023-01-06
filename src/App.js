6aad4720dd046dce4d25e25bd5acdd46e1c8cd2f
Christian McNeil <christianmcneil@Christians-MBP.fios-router.home>
2023-01-05 21:23:07 -0500

    Add backgrounds to match the current weather forecast

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import moment from "moment/moment";
 import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
 
 function App() {
   //Grab our url and key from the enviroment file (.env)
@@ -36,64 +39,82 @@ function App() {
 
     //loop over each index and capitalize the first letter
     for (let i = 0; i < arr.length; i++) {
-      arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1)
+      arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
     }
-    return arr.join(" ")
+    return arr.join(" ");
   };
 
   return (
-    <div className="app">
-      <main>
-        <div className="search-box">
-          <input
-            className="search-bar"
-            type="text"
-            placeholder="Search..."
-            /* When user types in the searhbar it will be stored into the value */
-            onChange={(e) => setLocation(e.target.value)}
-            value={location}
-            onKeyDown={searchLocation}
-          ></input>
-        </div>
-        {/** I need to make a check when to make whether or not the object is undefined or not */}
-        {typeof data.main != "undefined" ? (
-          <div>
-            <div className="location-box">
-              <div className="location">
-                {data.name}, {data.sys.country}
-              </div>
-              <div className="date">
-                {moment().format("dddd, MMMM Do YYYY")}
-              </div>
-            </div>
-            <div className="weather-box">
-              <div className="forecast">
-                <div className="highlow">
-                  <p>
-                    High:<span>{tempConverter(data.main.temp_max)}°</span>
-                  </p>
-                  <p>
-                    Low:<span>{tempConverter(data.main.temp_min)}°</span>
-                  </p>
-                </div>
-                <div className="humidity">
-                  <p>Humidity:</p>
-                  <span>{data.main.humidity}%</span>
-                </div>
-              </div>
-              <div className="temperature">
-                {tempConverter(data.main.temp)}°F
-              </div>
-              <div className="description">
-                {capitalizeString(data.weather[0].description)}
-              </div>
-            </div>
+    //Depending on the current weather temperature the background will change
+
+    <>
+      {/**If we have not done a search location then it will return undefined with our default background */}
+      <div
+        className={
+          typeof data.main != "undefined"
+            ? data.main.temp > 20
+              ? "warm"
+              : "app"
+            : "app"
+        }
+      >
+        <main>
+          <div className="search-box">
+            <input
+              className="search-bar"
+              type="text"
+              placeholder="Search..."
+              /* When user types in the searhbar it will be stored into the value */
+              onChange={(e) => setLocation(e.target.value)}
+              value={location}
+              onKeyDown={searchLocation}
+            ></input>
           </div>
-        ) : (
-          ""
-        )}
-      </main>
-    </div>
+          {/** I need to make a check when to make whether or not the object is undefined or not */}
+          {typeof data.main != "undefined" ? (
+            <div>
+              <div className="location-box">
+                <div className="location">
+                  {data.name}, {data.sys.country}
+                </div>
+                <div className="date">
+                  {moment().format("dddd, MMMM Do YYYY")}
+                </div>
+              </div>
+              <div className="weather-box">
+                <div className="forecast">
+                  <div className="highlow">
+                    <p>
+                      High:<span>{tempConverter(data.main.temp_max)}°</span>
+                    </p>
+                    <p>
+                      Low:<span>{tempConverter(data.main.temp_min)}°</span>
+                    </p>
+                  </div>
+                  <div className="humidity">
+                    <p>Humidity</p>
+                    <span>{data.main.humidity}%</span>
+                  </div>
+                </div>
+                <div className="temperature">
+                  {tempConverter(data.main.temp)}°F
+                </div>
+                <div className="description">
+                  {capitalizeString(data.weather[0].description)}
+                </div>
+                <div className="weather-image">
+                  <img
+                    src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                  />
+                </div>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+        </main>
+      </div>
+    </>
   );
 }
 
